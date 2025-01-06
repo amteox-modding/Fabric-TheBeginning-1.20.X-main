09212471f29b80bd813598e8dd464967d59b6a4e
amteox <mateuszgracyasz@gmail.com>
2025-01-06 14:57:34 +0100

    Initial commit

---
+++ src/main/java/net/amteox/thebeginning/TheBeginningClient.java
@@ -0,0 +1,10 @@
+package net.amteox.thebeginning;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBeginningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/amteox/thebeginning/TheBeginningDataGenerator.java
@@ -0,0 +1,11 @@
+package net.amteox.thebeginning;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheBeginningDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
